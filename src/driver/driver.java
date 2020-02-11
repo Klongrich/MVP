9b3e3acfe91a741cfc746fde530c7e4f44d801c7
Kyle Longrich <longrichk@gmail.com>
2020-02-11 12:33:15 -0800

    updating driver to support bulk download of data

--- src/driver/driver.java
+++ src/driver/driver.java
@@ -2,6 +2,10 @@ package driver;
 
 import data.car;
 import data.carsAPI;
+import java.util.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import java.util.ArrayList;
 
@@ -10,15 +14,40 @@ public class driver {
     public static void main(String args[]) {
 
         ArrayList<car> carData = new ArrayList<car>();
+        ArrayList<ArrayList<car>> allCarData = new ArrayList<ArrayList<car>>();
+
         carsAPI conn = new carsAPI();
 
-        carData = conn.getData();
+        long startTime;
+        long endTime;
 
-        for (int i = 0; i < carData.size(); i++) {
-            System.out.println("Name: " + carData.get(i).getName());
-            System.out.println("Price: " + carData.get(i).getPrice());
-            System.out.println("Miles: " + carData.get(i).getMiles());
-            System.out.println();
+
+        for (int i = 1; i < 3; i++) {
+
+            startTime = System.nanoTime();
+
+            carData = conn.getData("https://www.cars.com/for-sale/searchresults.action/?dealerType=all&page=" + i + "&perPage=100&rd=100&searchSource=GN_REFINEMENT&sort=relevance&stkTypId=28881&zc=44011");
+            allCarData.add(carData);
+
+            endTime = System.nanoTime();
+            System.out.println(i + ": Duration: " + ((endTime - startTime) / 1000) + " seconds");
         }
+
+        System.out.println("test");
+        try {
+            FileWriter fw = new FileWriter("/home/kyle/myfiles/java/carProject/src/driver/carData.csv", true);
+            fw.write("Name,Price,Miles,Close\n");
+
+            for (int j = 0; j < allCarData.size(); j++) {
+                for (int x = 0; x < allCarData.get(j).size(); x++) {
+                    fw.write(allCarData.get(j).get(x).getName() + "," + allCarData.get(j).get(x).getPrice() + "," + allCarData.get(j).get(x).getMiles() + "\n");
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("test");
+
     }
 }
